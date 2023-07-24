8ce339bbb0cd038db0121bf2c6daee4d6f2f67bd
Dmytro Bagrii <dimich.dmb@gmail.com>
2023-07-24 03:47:41 +0300

    Fix linkage error against kernel_termios_to_user_termios()
    kernel_termios_to_user_termios() is weak now, replace it with copy_to_user().

--- vtty.c
+++ vtty.c
@@ -450,7 +450,7 @@ static ssize_t vtmx_read (struct file *filp, char __user *ptr, size_t size, loff
 				size--;
 
 				if(tag == TAG_SET_TERMIOS) {
-					copystatus = kernel_termios_to_user_termios((struct termios2 __user*)ptr, &port->oob_data.termios);
+					copystatus = copy_to_user(ptr, &port->oob_data.termios, sizeof(struct termios2));
 					ret += sizeof(struct termios2);
 				} else {
 					copystatus = copy_to_user(ptr, &port->oob_data, port->oob_size);
